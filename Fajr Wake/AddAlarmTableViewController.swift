a7f566d4841873549c4c69b22e5dcdd07ac85eba
Ali Mir <110abidi@gmail.com>
2016-06-21 13:13:12 +0430

    version 4.1.2 - added picker test

--- Fajr Wake/AddAlarmTableViewController.swift
+++ Fajr Wake/AddAlarmTableViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class AddAlarmTableViewController: UITableViewController {
+class AddAlarmTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
         
     @IBOutlet weak var alarmLabelDetail: UILabel!
+    @IBOutlet weak var prayerTimesPicker: UIPickerView!
+    var pickerData: [[String]] = [[String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Connect data:
+        self.prayerTimesPicker.delegate = self
+        self.prayerTimesPicker.dataSource = self
+        
+        pickerData = [["1min", "2min", "3min"],
+                      ["Before", "At", "After"],
+                      ["Fajr", "Sunrise"]]
     }
+    
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return pickerData.count
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+    
+    
+    // ===================== TESTING ===================
+
+    // ===================== TESTING ===================
+    
     
     override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         if section == 0 {
